faccced1456ff6f6099456de05a0a00e6ec156d1
Vincent Br <vincent0512@outlook.com>
2024-01-11 18:29:48 +0100

    Created GO websocket server

---
+++ socket/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Type      int   `json:"type"`
+	Timestamp int64 `json:"timestamp"`
+	Data      Data  `json:"data"`
+}
+
+type Data struct {
+}
+
+func main() {
+	wsUpgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := wsUpgrader.Upgrade(w, r, nil)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for {
+			var message Message
+
+			err = conn.ReadJSON(&message)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Println("Received:", message)
+
+			err = conn.WriteJSON(message)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+		}
+	})
+
+	http.ListenAndServe(":4000", nil)
+}
